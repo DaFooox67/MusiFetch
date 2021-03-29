1f1c599ce4dabe95c935ad41555001380fd00c60
GroGGy67 <arnaudschmeltz@outlook.fr>
2021-03-29 20:17:12 +0200

    ++

--- src/site/MusiFetch/MusiFetch/urls.py
+++ src/site/MusiFetch/MusiFetch/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('find/', views.find, name='find'),
     path('create/', views.create, name='create'),
     path('register/', views.register, name='register'),
-    path('connection/', views.connection, name='connection')
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout')
 ]
